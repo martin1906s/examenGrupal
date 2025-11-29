fb520c8811be52c4c11e194877bb4fe55564c060
BlackmanCub <pedroenriquerhrh@gmail.com>
2025-11-29 15:47:37 -0500

    se subieron nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { SingIn } from './components/sing-in/sing-in';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Maintenance } from './components/maintenance/maintenance';
 import { ErrorPage } from './components/error-page/error-page';
+import { CrearTareaComponent } from './components/crear-tarea/crear-tarea';
+import { EquipoLiderComponent } from './components/equipo-lider/equipo-lider';
 
 export const routes: Routes = [
     {path: '', component: Login},
@@ -11,6 +13,8 @@ export const routes: Routes = [
     {path: 'signIn', component: SingIn},
     {path: 'dashboard', component: Dashboard},
     {path: 'maintenance', component: Maintenance},
+    {path: 'crear-tarea', component: CrearTareaComponent},
+    { path: 'comunicacion', component: EquipoLiderComponent },
     {path: 'errorPage', component: ErrorPage},
     { path: '**', redirectTo: '/errorPage', pathMatch: 'full' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './components/login/login';
 import { Navbar } from "./components/navbar/navbar";
+import { CrearTareaComponent } from "./components/crear-tarea/crear-tarea";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Navbar],
+  imports: [RouterOutlet, Login, Navbar, CrearTareaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
